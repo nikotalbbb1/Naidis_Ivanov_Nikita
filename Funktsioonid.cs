c8be14a396f525ffdbd6fac8907eac0e454fc2bb
opilane <opilane@HP-CZC2349HTB>
2026-02-05 12:17:57 +0200

    Add project files.

---
+++ Funktsioonid.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Naidis_Ivanov_Nikita
+{
+    public class Funktsioonid
+    {
+        public static float Kalkulaator(float arv1, float arv2)
+        {
+            return arv1 + arv2;
+        }
+    }
+
+}
---
+++ StartPage.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Channels;
+
+namespace Naidis_Ivanov_Nikita
+{
+    public class StartPage
+    {
+        public static void Main(string[] args)
+        {
+            Console.OutputEncoding = Encoding.UTF8;
+            Console.ForegroundColor = ConsoleColor.Blue;
+            Console.WriteLine("d");
+            
+            try
+            {
+                Console.WriteLine("Arv1:");
+                float a = float.Parse(Console.ReadLine());
+                Console.WriteLine("Arv 2:");
+                float b = float.Parse(Console.ReadLine());
+                float vastus = Funktsioonid.Kalkulaator(a,b);
+                Console.WriteLine(vastus);
+            }
+            catch (Exception e)
+            {
+
+                Console.WriteLine(e);
+            }
+        }
+    }
+
+}
