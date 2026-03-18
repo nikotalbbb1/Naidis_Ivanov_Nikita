a1b90761f766b47c9146bf5d209494497c891698
Opilane TTHK <opilane@tthk.ee>
2026-03-18 13:02:10 +0200

    .

--- Osa5.cs
+++ Osa5.cs
@@ -39,23 +39,69 @@ namespace Naidis_Ivanov_Nikita
         public static void Tuple()
         {
             Tuple<float, char> route = new Tuple<float, char>(2.5f, 'N');
-            Console.WriteLine($"Vahemaa: {route.Item1}, Suund: {route.Item2}");
+            Console.WriteLine($"Vahemaa: {route.Item1} , Suund: {route.Item2}");
         }
         public static void List()
         {
-            List<Person> people = new List<Person>();
-            people.Add(new Person() { Name = "Kadi" });
-            people.Add(new Person() { Name = "Mirje" });
+            
+            List<string> people = new List<string>() { "Kadi", "Mirje", "Liisa", "Aino", "Kadri", "Tõnis", "Märt", "Jaan", "Peeter", "Andres" };
 
-            foreach (Person p in people)
-                Console.WriteLine(p.Name);
+            //add
+            people.Add("Test");
+            Console.WriteLine(people);
+            foreach (string p in people)
+            {
+                Console.WriteLine(p);
+            }
+            //addRange
+            people.AddRange("t1", "t2");
+            foreach (string p in people)
+            {
+                Console.WriteLine(p);
+            }
+            //IndexOf
+            people.IndexOf("Liisa");
+
+            //Insert
+            people.Insert(0, "test1");
+
+            //remove
+            people.Remove("Aino");
+            foreach (string p in people)
+            {
+                Console.WriteLine(p);
+            }
+
+            //Sort
+            people.Sort((a, b) => a.Length.CompareTo(b.Length));
+
+            foreach (string p in people)
+            {
+                Console.WriteLine(p);
+            }
+
+            //BinarySearch()
+            int test = people.BinarySearch("Mirje");
+            Console.WriteLine(test);
+
+            //removeAt()
+            people.RemoveAt(0);
+            Console.WriteLine(people);
+            foreach (string p in people)
+            {
+                Console.WriteLine(p);
+            }
+            //FindIndex()
+            int p1 = people.FindIndex(p => p == "Mirje");
+            Console.WriteLine(p1);
+
         }
         public static void LinkedList()
         {
             LinkedList<int> loetelu = new LinkedList<int>();
             loetelu.AddLast(5);
             loetelu.AddLast(3);
-            loetelu.AddFirst(0);
+            loetelu.AddFirst(1);
 
             foreach (int arv in loetelu)
                 Console.WriteLine(arv);
@@ -67,6 +113,7 @@ namespace Naidis_Ivanov_Nikita
             
 
         }
+
 
 
 
